ab53c6c01c8f4640bd13d94790c3ea462e2eaf78
Philip Crotwell <crotwell@seis.sc.edu>
2021-09-02 10:08:35 -0400

    initial struct merge

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+use serde_json::Value;
+
+struct MSeed3Header {
+    record_indicator: String,
+    format_version: u8,
+    flags: u8,
+    nanosecond: u32,
+    year: u16,
+    day_of_year: u16,
+    hour: u8,
+    minute: u8,
+    second: u8,
+    encoding: u8,
+    sample_rate_period: i64,
+    num_samples: u32,
+    crc: u32,
+    publication_version: u8,
+    identifier_length: u8,
+    extra_headers_length: u16,
+    data_length: u32,
+}
+
+struct MSeed3Record {
+    header: MSeed3Header,
+    identifier: String,
+    extra_headers: Value,
+    data: Vec<u8>,
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
